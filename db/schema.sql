95a1a4e5b4fa3ae515ca2d2c344dc052b9b88107
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 00:50:12 -0700

    Added an error handler to catch any duplicate pairs.

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE tracks (
 
 CREATE TABLE playlists_tracks (
     id              serial PRIMARY KEY,
-    playlist_id     integer UNIQUE NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
-    track_id        integer UNIQUE NOT NULL REFERENCES tracks(id) ON DELETE CASCADE
+    playlist_id     integer NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+    track_id        integer NOT NULL REFERENCES tracks(id) ON DELETE CASCADE
+    UNIQUE(playlist_id, track_id)
 );
